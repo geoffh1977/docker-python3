4ba949c24aa39c2c650857934791ac5a4faa9e84
Geoffrey Harrison <geoffh1977@gmail.com>
2019-05-08 16:54:45 +1000

    Modified Develop Update

--- scripts/update_repo.sh
+++ scripts/update_repo.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 # Update The Git Repository Automatically
+LOCAL_VERSION="latest"
+APP_NAME="$(grep "appName" config.yml | cut -d: -f 2- | sed 's/^ //g')"
 
 # Update Local Repository Tags
 git fetch --tags
 echo
 
+# Check If Version Already Exists In Repository
+if git tag | grep -q "${LOCAL_VERSION}"
+then
+  echo "Version ${LOCAL_VERSION} Already Exists In Repository - No Action Required"
+  exit 1
+fi
+
 # Update Git With New Version
 echo "Updating Repository..."
-echo "  Updating Master Branch"
-git checkout master
+echo "  Create Release Branch"
+git branch "release/${LOCAL_VERSION}"
+git checkout "release/${LOCAL_VERSION}"
 git add -A :/
-git commit -m "Automated: Updating Master Branch"
+git commit -m "Automated: Create Release"
+
+echo "  Merge Back Into master Branch"
+git checkout master
+git merge "release/${LOCAL_VERSION}" --no-edit
+
+echo "  Remove Release Branch"
+git branch -d "release/${LOCAL_VERSION}"
 
 echo
 echo "  Pushing Updates To Repository"
